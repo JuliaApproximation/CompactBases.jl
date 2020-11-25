20282087fe2fe9838dda74d8e02f6076ca817f4e
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2020-11-25 19:12:33 +0100

    Also compare indices when comparing restricted bases

--- src/restricted_bases.jl
+++ src/restricted_bases.jl
@@ -15,7 +15,7 @@ unrestricted_basis(R::AbstractQuasiMatrix) = R
 unrestricted_basis(R::RestrictedQuasiArray) = parent(R)
 
 ==(A::BasisOrRestricted, B::BasisOrRestricted) =
-    unrestricted_basis(A) == unrestricted_basis(B)
+    unrestricted_basis(A) == unrestricted_basis(B) && indices(A,2) == indices(B,2)
 
 Base.hash(B::RestrictedQuasiArray, h::UInt) = hash(indices(B,2), hash(parent(B), h))
 
